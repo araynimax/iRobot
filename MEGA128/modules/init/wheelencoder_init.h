0877f6f860928dc40ddc49fe5385a731ae7ade36
ArayniMax <arayni.max@gmail.com>
2017-12-28 15:45:42 +0100

    wheelencoderstruct hat jetzt eine kopie pro seite

--- MEGA128/modules/init/wheelencoder_init.h
+++ MEGA128/modules/init/wheelencoder_init.h
@@ -1,21 +1,28 @@
 struct structWheelEncoder{
   int left;
   int right;
+  int leftMove;
+  int rightMove;
 } wheelEncoder;
+
 
 // External Interrupt 4 Wheel encoder Left
 interrupt [EXT_INT4] void ext_int4_isr(void)
 {
  wheelEncoder.left++;
+ wheelEncoder.leftMove++;
 }
 
 // External Interrupt 6 Wheel encoder Right
 interrupt [EXT_INT6] void ext_int6_isr(void)
 {
   wheelEncoder.right++;
+  wheelEncoder.rightMove++;
 }
 
 void wheelencoder_init(){
  wheelEncoder.left = 0;
  wheelEncoder.right = 0;
+ wheelEncoder.leftMove = 0;
+ wheelEncoder.rightMove = 0;
 }
